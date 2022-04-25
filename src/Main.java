5612639ac92c1761c6e426c5f4b38a2668a94885
Maria Negretskaya <marian.93@mail.ru>
2022-04-25 20:27:48 +0300

    Calculation of bonus

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+int primaryBalance = 100;
+int sumOfFirstRefill = 100;
+int sumOfSecondRefill = 1100;
+        System.out.println("Итоговый счет");
+        System.out.println((primaryBalance+sumOfFirstRefill) + " руб ");
+
+        System.out.println("Количество бонусных рублей");
+        System.out.println((sumOfSecondRefill/100) + " руб ");
+    }
+}
